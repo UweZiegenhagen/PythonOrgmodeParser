b28429382807cbf56d196d9ba5b83f52cd705790
Uwe Ziegenhagen <UweZiegenhagen@users.noreply.github.com>
2017-06-28 05:49:16 +0200

    Initial commit

---
+++ OrgModeParser.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import re
+ 
+def parseEmaceOrgmode(s):
+    r = '^([\*]+)?\s?(TODO|PROGRESSING|FEEDBACK|VERIFY|POSTPONED|DELEGATED|CANCELLED|DONE)?\s?(\[#[A|B|C]\])?\s?(.*?)\s*(:(.*):)?$'    
+    m = re.search(r,s)
+    level = m.group(1)
+    if (level is not None):
+        level = len(level)
+    prio = m.group(3)
+    if (prio is not None):
+        prio = prio[2:3]
+    tags = []
+    a = m.group(5)
+    if a != None:
+        b = len(a)-1
+        a= a[1:b]
+        a = a.split(':')
+    tags.append(a)
+    return(level, m.group(2), prio, m.group(4), tags)
+ 
+with open("../orgmode.org", "r") as ins:
+    for line in ins:
+        level, status, priority, task, tags = parseEmaceOrgmode(line)
+        if level is not None:        
+            print('Level:', level)
+            print('Status:', status)
+            print('Priority:', priority)
+            print('Task:', task)
+            print('Tags:',tags,'\n\n')
